54024aea910570e1ccc018b972ebcfbbfd29ed79
Luzheng Li <42434373+luzhengli@users.noreply.github.com>
2024-06-13 13:54:10 +0800

    Update chatbot_demo.py

--- chatbot_demo.py
+++ chatbot_demo.py
@@ -1,5 +1,5 @@
 # from openai import OpenAI
-from http import client
+# from http import client
 import streamlit as st
 from langchain_community.llms import Tongyi
 from langchain_core.runnables import ConfigurableField
